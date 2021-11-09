8acbdd8fee1528f9d64fba40c6789358d6486c07
darkoohrid <darko.lamanovski@hotmail.com>
2021-11-09 19:57:55 +0100

    Weather component funcionalities, Connection with API, Basic Bootstrap styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
 <div class="header">
     <!--  bg-color:# b2cced scss variable in bootstrap ??? -->
     <div class="fixed-top text-center bg-primary ">
-        <a class="navbar-brand text-warning" href="/">Weather Forecast</a>
+        <a class="navbar-brand text-warning" href="/"><h1>Weather Forecast Maribor</h1></a>
     </div>
 </div>
 
--- src/components/WeatherComponent.vue
+++ src/components/WeatherComponent.vue
@@ -1,26 +1,66 @@
 <template>
-<div class="container p-2">
-    <div class="my-container m-5">
-        <div class="m-5">
-            <h1 class="text-center">Weather Forecast Maribor</h1>
-            <p class="text-center">Coordinates: x: , y:</p>
-        </div>
-
-        <div class="row my-row">
+<div class="container">
+    <div class="my-container">
+        <div class=" row my-row">
             <div class="col my-col">
-                <p>Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
+                    <div>
+                        <p class="text-center"><b>Timezone: </b>{{forecast.timezone}}</p>
+                        <!-- <p class="text-center">Coordinates: lat: {{forecast.lat}} , lon: {{forecast.lon}}</p>                 -->                 
+                    </div>
+                    <div class="text-center">
+                        <button @click="getForecast()" class="btn btn-primary">Update Forecast info</button>
+                    </div>
+                    <div>                    
+                        <p class="text-center"><span><b>Obtained on:</b> </span> {{getDateMethod().toLocaleString()}}</p>
+                    </div>
             </div>
         </div>
-        <div class="row my-row mt-2">
-            <div class="col-md-6 col-sm-12 my-col">
-                Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+        <div class="row my-row mt-1">
+            <div class="col-md-6 col-sm-12 my-col mb-1 mt-1">
+              <div>
+                  <div>
+                      <h3>Maribor, Sl</h3>
+                      <p><span>{{getDateMethod().toDateString()}}</span></p>
+                  </div>
+                  <div>
+                    <h3 class="text-center">Current temp: {{forecast.current.temp}} °</h3>
+                  </div>
+                  <div class="text-center">
+                    <!-- <img src="http://openweathermap.org/img/wn/04d@2x.png"> -->
+                    <img :src="'http://openweathermap.org/img/w/' + forecast.current.weather[0].icon + '.png' ">
+                  </div>
+                  <div>
+                    <p class="text-center"><b>Description:</b> {{forecast.current.weather[0].main}}, {{forecast.current.weather[0].description}}.</p>
+                  </div>
+                  <div class="text-center">
+                    <p><b>Feels like: </b>{{forecast.current.feels_like}} ° </p>
+                    <p><b>Pressure: </b>{{forecast.current.pressure}} hPa </p>
+                    <p><b>Humidity: </b>{{forecast.current.humidity}} % </p>
+                  </div>
+
+              </div>
             </div>
-            <div class="col-md-6 col-sm-12 my-col">
-                <div>Div 1</div>
-                <div>Div 2</div>
-                <div>Div 3</div>
-                <div>Div 4</div>
-                <div>Div 5</div>
+            <div class="col-md-6 col-sm-12 my-col mb-1 mt-1">
+                <div v-for="index in 5" :key="index" class="row border">
+                    <div class="col">
+                        <p class="text-center">{{(getDateMethod(forecast.daily[index-1].dt * 1000)).toDateString()}}</p>
+                    </div>
+                    <div class="col">
+                        <span><img style="width:80px; height:80px" :src="'http://openweathermap.org/img/w/' + forecast.daily[index-1].weather[0].icon + '.png' "/></span>                       
+                    </div>
+                    <div class="col">
+                        <p>
+                            <span>Min:{{Math.round(forecast.daily[index-1].temp.min)}} ° </span> 
+                        </p>
+                        <p class="">
+                            <span>Max:{{Math.round(forecast.daily[index-1].temp.max)}} °  </span>  
+                        </p>
+  
+                    </div>
+                    <div class="col">
+                        <p class=""><b>Description:</b> {{forecast.daily[index-1].weather[0].main}} </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -31,14 +71,37 @@
 
 <script>
 export default {
-
+    name:"WeatherComponent",
+    data(){
+        return {
+            api_key: "7ca6e152197750c5f2ed06aa6d6c687c",
+            forecast: {}
+        }
+    },
+    methods: {
+        getForecast(){
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=46.55&lon=15.64&units=metric&exclude=hourly,minutely&appid=7ca6e152197750c5f2ed06aa6d6c687c`)
+            .then(res => res.json())
+            .then(data=>
+            this.forecast=data); 
+        },
+        getDateMethod(){
+            return new Date();
+        },
+    },
+    computed:{
+        
+    },
+    created(){
+        this.getForecast();
+        console.log(this.forecast);
+    }
 }
 </script>
 
 <style scoped>
-.my-container .container{
-    border:1px solid green;
-    margin:auto;
+.my-container{
+    margin:10%;
 }
 .my-row{
     border:3px solid red;
