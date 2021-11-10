4d19a3aef201038c6c47eb4b42daa91c4b5c2d8f
darkoohrid <darko.lamanovski@hotmail.com>
2021-11-10 11:44:27 +0100

    Simple Loader Implemented

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <Header></Header>
-  <WeatherComponent></WeatherComponent>
+  <WeatherComponent v-if="isDataLoaded"></WeatherComponent>
+  <Loader v-else></Loader>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import WeatherComponent from "./components/WeatherComponent.vue";
+import Loader from "./components/Loader.vue"
 
 export default {
+  name:"app",
+  data(){
+    return {
+      isDataLoaded:false,
+      forecast:{}
+    }
+  },
+  methods:{
+    getForecast(){
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=46.55&lon=15.64&units=metric&exclude=hourly,minutely&appid=7ca6e152197750c5f2ed06aa6d6c687c`)
+      .then(res => res.json())
+      .then(data=>
+      this.forecast=data); 
+      }    
+  },
   components: {
     Header,
-    WeatherComponent   
+    WeatherComponent,
+    Loader   
+  },
+  created(){
+    this.isDataLoaded = false;
+    setTimeout(() => {
+      this.getForecast();
+      this.isDataLoaded = true;
+      console.log("here");
+    }, 1000);
   }
 };
 </script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,14 @@
+<template>
+<img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e0b072897469.5bf6e79950d23.gif">
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+img{
+    width:100%;
+    height:100vh;
+}
+</style>
